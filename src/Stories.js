f4092fd0af100d104c229f94eca9586153602e10
Jéssica <jessicavitorsantos@outlook.com>
2022-05-09 15:38:05 -0300

    items no formato de arrays/objetos feito!

--- src/Stories.js
+++ src/Stories.js
@@ -14,7 +14,43 @@ function Storie (props) {
 
 
 export default function Stories () {
-    
+    const listaStories = [
+        {
+            fotoPerfil: "https://i.pinimg.com/474x/70/56/d7/7056d7fc62f9345b87ec224e12ad39e9--ginger-kitten-ginger-cats.jpg",
+            usuario: "9gag"
+        },
+        {
+            fotoPerfil: "https://i.pinimg.com/236x/62/ee/e0/62eee08d28589f947b6b4a67808f42d3.jpg",
+            usuario: "meowed"
+        },
+        {
+            fotoPerfil: "https://i0.wp.com/portalkpopbrasil.com/wp-content/uploads/2021/09/Guard-You-young-k.png?fit=811%2C450&ssl=1",
+            usuario: "barked"    
+        },
+        {
+            fotoPerfil: "https://www.curiosidades.com.br/wp-content/uploads/2020/09/Gatinho-vestido-de-Pikachu-1.jpg",
+            usuario:  "nathanwpyle..."  
+        },
+        {
+            fotoPerfil: "https://noticias.buscavoluntaria.com.br/wp-content/uploads/2019/10/@charles_felizardo.png",
+            usuario:  "wawawiwac..."
+        },
+        {
+            fotoPerfil: "https://www.taiyou.fr/an_content/_upload/img-prod/38657/4_1.jpg",
+            usuario:  "respondeai"  
+        },
+        {
+            fotoPerfil: "https://i.pinimg.com/originals/ab/21/d5/ab21d5b71ba7b3b74947c3e5656c6aee.jpg",
+            usuario:  "filomoderna"
+        },
+        {
+            fotoPerfil: "https://i.pinimg.com/736x/86/14/d4/8614d476baf92a2a40f3168544af7210.jpg",
+            usuario: "memeriagourmet"    
+        }
+    ]
+
+
+
     return (
         <div class="stories-box">
 
@@ -23,15 +59,7 @@ export default function Stories () {
             </div>
 
             <div class="todos-os-stories">
-                <Storie fotoPerfil="https://i.pinimg.com/474x/70/56/d7/7056d7fc62f9345b87ec224e12ad39e9--ginger-kitten-ginger-cats.jpg" usuario="9gag" />
-                <Storie fotoPerfil="https://i.pinimg.com/236x/62/ee/e0/62eee08d28589f947b6b4a67808f42d3.jpg" usuario="meowed" />
-                <Storie fotoPerfil="https://i0.wp.com/portalkpopbrasil.com/wp-content/uploads/2021/09/Guard-You-young-k.png?fit=811%2C450&ssl=1" usuario="barked" />
-                <Storie fotoPerfil="https://www.curiosidades.com.br/wp-content/uploads/2020/09/Gatinho-vestido-de-Pikachu-1.jpg" usuario="nathanwpyle..." />
-                <Storie fotoPerfil="https://noticias.buscavoluntaria.com.br/wp-content/uploads/2019/10/@charles_felizardo.png" usuario="wawawiwac..." />
-                <Storie fotoPerfil="https://www.taiyou.fr/an_content/_upload/img-prod/38657/4_1.jpg" usuario="respondeai" />
-                <Storie fotoPerfil="https://i.pinimg.com/originals/ab/21/d5/ab21d5b71ba7b3b74947c3e5656c6aee.jpg" usuario="filomoderna" />
-                <Storie fotoPerfil="https://i.pinimg.com/736x/86/14/d4/8614d476baf92a2a40f3168544af7210.jpg" usuario="memeriagourmet" />
-
+                {listaStories.map(lista => <Storie fotoPerfil={lista.fotoPerfil} usuario={lista.usuario}/>)}
             </div>
 
         </div>
